fed9e490fa65a7b6a576fb2ae0ecc82a0373ffd9
Rannie Ollit <rannie.ollit@petnet.com.ph>
2021-08-17 18:05:37 +0800

    Added minting

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -16,8 +16,33 @@ const getTotalSupply = async (req, res) => {
     })
 }
 
+const getBalances = async (req, res) => {
+    const contract = await productContract();
+    const balance = await contract.methods.getBalances().call();
+    res.json({
+        balance
+    })
+}
+
+const mintProduct = async (req, res) => {
+    try {
+        const contract = await productContract();
+        await contract.methods.mint(req.body.name).encodeABI();
+        return res.json({
+            message: 'successfully minted!'
+        });
+        // return
+    } catch (error) {
+        return res.status(400).json({
+            message: error.message
+        })
+    }
+}
+
 
 export {
     getProducts,
-    getTotalSupply
+    getTotalSupply,
+    mintProduct,
+    getBalances
 }
--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { getProducts, getTotalSupply } from '../controllers/product.controller'
+import { getProducts, getTotalSupply, mintProduct } from '../controllers/product.controller'
 
 const router = express.Router()
 
 router.get('/', getProducts);
 router.get('/supply/total', getTotalSupply);
+router.post('/mint', mintProduct);
 
 export default router;
