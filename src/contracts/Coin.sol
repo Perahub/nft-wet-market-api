8ae8f284c980f1557ec45f65a82a6677f975c6ae
Rannie Ollit <rannie.ollit@petnet.com.ph>
2021-08-25 09:32:02 +0800

    Added initial value of coin

--- src/contracts/Coin.sol
+++ src/contracts/Coin.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.4.22 <0.9.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Coin is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Token", "TKN") {
+    constructor(uint256 initialSupply) ERC20("PHP Coin", "PHPC") {
         _mint(msg.sender, initialSupply);
     }
 }
